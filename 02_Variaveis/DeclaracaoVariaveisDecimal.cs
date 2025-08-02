3483a2f1dd407b233f0ff50060675f4054cc8622
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-08-01 22:07:36 -0300

    Declaração variáveis decimal

---
+++ 02_Variaveis/DeclaracaoVariaveisDecimal.cs
@@ -0,0 +1,13 @@
+using System;
+
+class DeclaracaoVariaveisDecimal
+	{ 
+		static void Main(String[] args)
+		{
+			//decimal myMoneyError = 300.0;
+			decimal myMoneyCorrect = 300.0m;
+			Console.WriteLine(myMoneyCorrect);
+			
+		}
+	
+	}

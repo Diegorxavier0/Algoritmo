e80e83e11c0de5272ed691c736724775bb3f0dd2
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-08-01 21:45:25 -0300

    Declaração de variáveis

---
+++ 02_Variaveis/DeclaracaoVariaveis.cs
@@ -0,0 +1,29 @@
+using  System;
+
+class DeclaracaoVariaveis
+{
+		static void Main ()
+		{
+			/*
+			Declaração de variável
+			*/
+			int a;
+			
+			//Inicialização da Variável
+			a = 22;
+			Console.WriteLine (a);
+			
+			//Reatribuição de Valor na Variável
+			a = 56;
+			Console.WriteLine (a);
+			
+			//Outras formas de declaração de variáveis
+			int b = 925;
+			Console.WriteLine (b);
+			
+			int y = 10, x = 20;
+			int z = y + x;
+			Console.WriteLine (z);
+			
+		}
+}

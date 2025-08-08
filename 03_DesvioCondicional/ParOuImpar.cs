3337ecc6fb277d1b652703339726c13c5737a305
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-08-07 21:36:57 -0300

    Valida se o número é par ou impar

---
+++ 03_DesvioCondicional/ParOuImpar.cs
@@ -0,0 +1,23 @@
+using System;
+
+class ParOuImpar
+{
+    static void Main()
+    {
+		int numero;
+		
+        Console.Write("Digite um número: ");
+		
+        // Lê o número digitado e converte para int
+		numero = int.Parse(Console.ReadLine()); 
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+    }
+}

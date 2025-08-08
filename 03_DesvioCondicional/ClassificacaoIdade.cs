0efbb12405cd7682254a2cc090c6ecba262c665c
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-08-07 21:39:16 -0300

    mostra ao usuário sua idade e sua Classificação

---
+++ 03_DesvioCondicional/ClassificacaoIdade.cs
@@ -0,0 +1,28 @@
+using System;
+
+class ClassificacaoIdade
+{
+    static void Main()
+    {
+        int idade;
+        Console.WriteLine("Digite sua idade");
+        idade = int.Parse(Console.ReadLine());
+
+        if (idade <= 12)
+        {
+            Console.WriteLine("Você é Criança");
+        }
+        else if (idade <= 17)
+        {
+            Console.WriteLine("Você é Adolescente");
+        }
+        else if (idade <= 59)
+        {
+            Console.WriteLine("Você é Adulto");
+        }
+        else 
+        {
+            Console.WriteLine("Você é Idoso");
+        }
+    }
+}

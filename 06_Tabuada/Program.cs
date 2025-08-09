85b94315455adca2fbf3b095cea91a2a257736e5
Diego Rodrigues Xavier <diegorodriguesxavier970@gmail.com>
2025-08-08 22:06:40 -0300

    Tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Digite um número para ver sua tabuada: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tabuada do {n}:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+              
+                int resultado = n * i; 
+                Console.WriteLine($"{n} x {i} = {resultado}");
+            }
+        }
+    }
+}
